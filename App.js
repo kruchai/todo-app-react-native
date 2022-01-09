d4fa4d91ec6da42143700be3ce3ab809b0ab1b04
Oleg Kruchay <okruchay@gmail.com>
2022-01-09 18:50:50 +0200

    feature(App.js): attaching the new components, using Platform for some sensitive cases
    v0.1: Settings and implementation

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet, SafeAreaView, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Platform, StyleSheet, SafeAreaView, Text, View } from 'react-native'
+import { Header } from './components/Header'
+import { ToDo } from './components/ToDo'
 
 export default function App() {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
+        <Header />
+        <ToDo />
         <Text style={styles.text}>{ 'It works' }</Text>
         <StatusBar style="auto" />
       </View>
@@ -17,12 +21,17 @@ const styles = StyleSheet.create({
     backgroundColor: Platform.OS !== 'ios' ? 'red' : 'green',
   },
   container: {
-    height: '100%',
+    height: Platform.OS === 'web' ? '100vh' : '100%',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
     backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
+    flex: 1,
+    width: '100%',
+    textAlign: 'center',
+    backgroundColor: '#e9e9e9',
     color: Platform.OS !== 'ios' ? 'red' : 'green',
   }
 });
